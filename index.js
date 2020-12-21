5d9f7e451e71a1e477f00bc21176d58a3865c697
Benjamin Wong <ikatadrop@gmail.com>
2020-12-20 21:32:50 -0800

    fixed static port

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const router = express.Router();
 const app = express();
 var port = 5000;
 
-app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static(path.join(__dirname, "/client")));
 
 app.get('/', function(req, res){
    res.sendFile(path.join(__dirname + "/client/home.html"));
 });
 
 app.use("/", router);
+
+app.set('port', process.env.PORT || 3000);
 app.listen(port);
 console.log(`Started on port ${port}`);
